aafd49a8d81546805a56880ff3bf4617bd025eba
Bagou Ines <inesbagou45@gmail.com>
2025-12-16 21:00:47 +0100

    ajout header footer a la page profil

--- smartdish/src/pages/Profil.js
+++ smartdish/src/pages/Profil.js
@@ -1,4 +1,6 @@
 import React, { useMemo, useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./Profil.css";
 
 export default function Profile() {
